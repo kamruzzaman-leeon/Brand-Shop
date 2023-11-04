14a305a4512a4bdf8da50dd13b3b4e8c100921c8
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-04 18:45:57 +0600

    fix the rating input & state from update page

--- src/page/ProductUpdate.jsx
+++ src/page/ProductUpdate.jsx
@@ -1,24 +1,41 @@
-
+import { useEffect, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 const ProductUpdate = () => {
     const pro = useLoaderData();
-    // console.log(pro);
     const { _id, product, productImageUrl, brand, productType, price, rating, description } = pro;
+
+    const [updateProduct, setUpdateProduct] = useState({
+        product,
+        productImageUrl,
+        brand,
+        productType,
+        price,
+        rating,
+        description,
+    });
+    useEffect(() => {
+        // Update the rating when the pro data changes
+        setUpdateProduct((prevUpdateProduct) => ({
+            ...prevUpdateProduct,
+            rating: rating,
+        }));
+    }, [rating]);
+
     const handleUpdateProduct = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         const form = e.target;
         const product = form.product.value;
         const productImageUrl = form.product_image_url.value;
         const brand = form.brand.value.toUpperCase();
         const productType = form.product_type.value;
         const price = form.price.value;
-        const rating = form.rating.value;
+        // Get the rating value from the selected radio button
+        const rating = form.querySelector('input[name="rating"]:checked').value;
         const description = form.description.value;
 
-        const updateProduct = { product, productImageUrl, brand, productType, price, rating, description }
-        // console.log(updateProduct)
+        const updatedProduct = { product, productImageUrl, brand, productType, price, rating, description };
 
         // send data to server
         fetch(`http://localhost:5000/productdetails/${_id}`, {
@@ -26,7 +43,7 @@ const ProductUpdate = () => {
             headers: {
                 'content-type': 'application/json'
             },
-            body: JSON.stringify(updateProduct)
+            body: JSON.stringify(updatedProduct)
         })
             .then(res => res.json())
             .then(data => {
@@ -36,11 +53,11 @@ const ProductUpdate = () => {
                         text: 'Successfully Product Updated!',
                         icon: 'success',
                         confirmButtonText: 'Cool'
-                    })
-
+                    });
                 }
-            })
+            });
     }
+
 
     return (
         <div className='shadow-xl p-24 container mx-auto'>
@@ -111,21 +128,65 @@ const ProductUpdate = () => {
                                 className="input input-bordered w-full" required />
                         </label>
                     </div>
-                    <div className="form-control md:w-1/2 mx-4">
 
-                        <label className="label">
-                            <span className="label-text">Rating</span>
+                    <div className='form-control md:w-1/2 mx-4'>
+                        <label className='label'>
+                            <span className='label-text'>Rating</span>
                         </label>
-                        <label className="input-group">
-                            <div className="rating rating-lg">
-                                <input type="radio" name="rating" value="0" id="0" className="rating-hidden" />
-                                <input type="radio" name="rating" value="1" id="1" className="mask mask-star-2" />
-                                <input type="radio" name="rating" value="2" id="2" className="mask mask-star-2" />
-                                <input type="radio" name="rating" value="3" id="3" className="mask mask-star-2" />
-                                <input type="radio" name="rating" value="4" id="4" className="mask mask-star-2" />
-                                <input type="radio" name="rating" value="5" id="5" className="mask mask-star-2" />
+                        <label className='input-group'>
+                            <div className='rating rating-lg'>
+                                <input
+                                    type='radio'
+                                    name='rating'
+                                    value='0'
+                                    id='0'
+                                    className='rating-hidden'
+                                    defaultChecked={updateProduct.rating === '0'}
+                                />
+                                <input
+                                    type='radio'
+                                    name='rating'
+                                    value='1'
+                                    id='1'
+                                    className='mask mask-star-2'
+                                    defaultChecked={updateProduct.rating ==='1'}
+                                />
+                                <input
+                                    type='radio'
+                                    name='rating'
+                                    value='2'
+                                    id='2'
+                                    className='mask mask-star-2'
+                                    defaultChecked={updateProduct.rating === '2'}
+                                />
+                                <input
+                                    type='radio'
+                                    name='rating'
+                                    value='3'
+                                    id='3'
+                                    className='mask mask-star-2'
+                                    defaultChecked={updateProduct.rating === '3'}
+                                />
+                                <input
+                                    type='radio'
+                                    name='rating'
+                                    value='4'
+                                    id='4'
+                                    className='mask mask-star-2'
+                                    defaultChecked={updateProduct.rating === '4'}
+                                />
+                                <input
+                                    type='radio'
+                                    name='rating'
+                                    value='5'
+                                    id='5'
+                                    className='mask mask-star-2'
+                                    defaultChecked={updateProduct.rating === '5'}
+                                />
                             </div>
                         </label>
+
+
                     </div>
 
 
